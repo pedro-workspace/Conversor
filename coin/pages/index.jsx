2f3f3f7daf20f87088838fd8c80248a932a22e89
Pedro Souza <pedroworkspace@gmail.com>
2022-09-10 17:15:25 -0300

    Testes findados, funcionalidades OK

--- coin/pages/index.jsx
+++ coin/pages/index.jsx
@@ -1,55 +1,50 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Conversor.module.css'
 import { useState } from 'react'
 
 export default function Conversor() {
 
-  const [select, setSelect] = useState('metro') //definir a unidade de conversão
-  const [numero, setNumero] = useState(0) //definir o valor de conversão
+  const [inSelect, setInSelect] = useState('metro') //definir a unidade de conversão
+  const [entrada, setEntrada] = useState(0) //definir o valor de conversão
 
   const [outSelect, setOutSelect] = useState('metro')
   const [saida, setSaida] = useState(0)
 
-  const Converter = (inSelect = select, outSelect = outSelect, input = numero) => {
-    //função que confere as unidades de saída e muda o valor global saida
-
+  const Converter = () => {
+  
     if ( inSelect== "centimetro") {
       if (outSelect == "metro") {
-        saida = parseFloat(input / 100).toFixed(2)
+        setSaida(parseFloat(entrada / 100).toFixed(2))
       }
       else if (outSelect == "kilometro") {
-        saida = parseFloat(input / 100000).toFided(6)
+        setSaida(parseFloat(entrada / 100000).toFided(6))
       }
     }else if (inSelect == "metro") {
       if (outSelect == "centrimetro") {
-        saida = parseFloat(input * 100).toFixed(2)
+        setSaida(parseFloat(entrada * 100).toFixed(2))
       }
       else if (outSelect == "kilometro") {
-        saida = parseFloat(input / 1000).toFixed(3)
+        setSaida(parseFloat(entrada / 1000).toFixed(3))
       }
     }
     else if (inSelect == "kilometro") {
       if (outSelect == "metro") {
-        saida = parseFloat(input * 1000).toFixed(2)
+        setSaida(parseFloat(entrada * 1000).toFixed(2))
       }
       else if (outSelect == "centimetro") {
-        saida = parseFloat(input * 100000).toFixed(2)
+        setSaida(parseFloat(entrada * 100000).toFixed(2))
       }
     }
 
-    return setSaida(saida)
-  }
-
-  const Resultado = (retorno) => {
-
     return (
       <div>
-        <span>{retorno}</span>
+        <span>{saida}</span>
       </div>
     )
-  }
+  } 
 
+  
   return (
     <div>
       <h1>Prototipo inicial, conversor de unidades</h1>
@@ -58,26 +53,34 @@ export default function Conversor() {
         <h4>Conversor</h4>
         <h3>Unidades de comprimento</h3>
         <div>
-          <label htmlFor="">de <input placeholder='0' type="number" /></label>
-          <select value={select}
-           onChange={(e)=>{setSelect(e.target.value)
-          console.log(select)}}
-            placeholder='Escolha a unidade' name="entrada" id="entrada">
+          
+          <label htmlFor=""> <input value={entrada} 
+           onChange={(e) => {setEntrada(e.target.value)}} placeholder='0' type="number" /> </label>
+
+          <select value={inSelect}
+           onChange={(e)=>{setInSelect(e.target.value)
+           console.log(inSelect)
+           console.log(entrada)
+          }}
+             name="entrada" id="entrada">
             <option value="centimetro">centímetros</option>
             <option value="metro">metros</option>
             <option value="kilometro">kilômetros</option>
           </select>
-          <span> para: </span>
+
+          <Converter></Converter>
+
           <select value={outSelect} onChange={(e) => {
             setOutSelect(e.target.value)
+            console.log(outSelect)
+            console.log(saida)
           }}  name="saida" id="saida">
             <option value="centimetro">centímetros</option>
             <option value="metro">metros</option>
             <option value="kilometro">kilômetros</option>
           </select>
+
         </div>
-        <button></button>
-        <Resultado></Resultado>
       </div>
     </div>
   )
